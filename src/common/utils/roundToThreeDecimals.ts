87c7b0634f03c242f0bd71b1a9b7a2de92d006f9
unknown <vladrework@gmail.com>
2024-07-11 05:59:14 -0700

    feat: roundToThreeDecimals

---
+++ src/common/utils/roundToThreeDecimals.ts
@@ -0,0 +1,3 @@
+export const roundToThreeDecimals = (number: number) => {
+    return Number(number.toFixed(3))
+}
--- src/currencyTable/currencyTable.tsx
+++ src/currencyTable/currencyTable.tsx
@@ -1,6 +1,7 @@
 import './currencyTable.css'
 import {MinValueHighlight} from "../common/utils/minValueHighlight";
 import {usePollingRequest} from "../common/hooks/useLongpolling";
+import {roundToThreeDecimals} from "../common/utils/roundToThreeDecimals";
 
 export const CurrencyTable = () => {
 
@@ -36,33 +37,54 @@ export const CurrencyTable = () => {
             <tr>
                 <td>RUB/CUPCAKE</td>
                 {MinValueHighlight([
-                firstRates.RUB,
-                secondRates.RUB,
-                thirdRates.RUB])}
+                    roundToThreeDecimals(firstRates.RUB),
+                    roundToThreeDecimals(secondRates.RUB),
+                    roundToThreeDecimals(thirdRates.RUB),
+                ])}
             </tr>
 
             <tr>
                 <td>USD/CUPCAKE</td>
                 {MinValueHighlight([
-                    firstRates.USD,
-                    secondRates.USD,
-                    thirdRates.USD])}
+                    roundToThreeDecimals(firstRates.USD),
+                    roundToThreeDecimals(secondRates.USD),
+                    roundToThreeDecimals(thirdRates.USD)])}
             </tr>
 
             <tr>
                 <td>EUR/CUPCAKE</td>
+                {MinValueHighlight([
+                    roundToThreeDecimals(firstRates.EUR),
+                    roundToThreeDecimals(secondRates.EUR),
+                    roundToThreeDecimals(thirdRates.EUR),
+                ])}
             </tr>
 
             <tr>
                 <td>RUB/USD</td>
+                {MinValueHighlight([
+                    roundToThreeDecimals(firstRates.RUB / firstRates.USD),
+                    roundToThreeDecimals(secondRates.RUB / secondRates.USD),
+                    roundToThreeDecimals(thirdRates.RUB / thirdRates.USD),
+                ])}
             </tr>
 
             <tr>
                 <td>RUB/EUR</td>
+                {MinValueHighlight([
+                    roundToThreeDecimals(firstRates.RUB / firstRates.EUR),
+                    roundToThreeDecimals(secondRates.RUB / secondRates.EUR),
+                    roundToThreeDecimals(thirdRates.RUB / thirdRates.EUR),
+                ])}
             </tr>
 
             <tr>
                 <td>EUR/RUB</td>
+                {MinValueHighlight([
+                    roundToThreeDecimals(firstRates.EUR / firstRates.RUB),
+                    roundToThreeDecimals(secondRates.EUR / secondRates.RUB),
+                    roundToThreeDecimals(thirdRates.EUR / thirdRates.RUB)
+                ])}
             </tr>
             </tbody>
         </table>
